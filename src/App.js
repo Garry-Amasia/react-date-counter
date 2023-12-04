8dacb54386a6f667b18596a8e7482942689b8e83
GarryAmasia <amasiagarry87@gmail.com>
2023-12-04 15:54:06 +1100

    mini project completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+  // console.log(date.toDateString());
+
+  const increase = () => setCount((current) => current + step);
+  const decrease = () => setCount((current) => current - step);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={decrease}>-</button>
+        <span>Count: {count} </span>
+        <button onClick={increase}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setStep((current) => current - 1)}>-</button>
+        <span>Step: {step} </span>
+        <button onClick={() => setStep((current) => current + 1)}>+</button>
+      </div>
+
+      <p>
+        {count === 0
+          ? "today is"
+          : count > 0
+          ? `${count} days from today`
+          : `${Math.abs(count)} days was`}
+        {date.toDateString()}
+      </p>
     </div>
   );
 }
